9995a1c903d9a8ba5fb4ec8bbc950d5bd6bfe8bd
SOJA_Dev <rudcksdid@naver.com>
2025-03-18 14:17:21 +0900

    원상복구

--- Assets/Scripts/UI/GameSetting/GameSetupPopup.cs
+++ Assets/Scripts/UI/GameSetting/GameSetupPopup.cs
@@ -10,6 +10,7 @@ namespace UI.GameSetting
     public class GameSetupPopup : MonoBehaviour
     {
         [SerializeField] private Button gameSettingButton;
+        [SerializeField] private Button gameStartButton;
 
         [SerializeField] private TMP_InputField roomNameInputField;
         [SerializeField] private TMP_Dropdown maxPlayersDropdown;
@@ -18,12 +19,20 @@ namespace UI.GameSetting
         [SerializeField] private Button confirmButton;
         [SerializeField] private Button cancelButton;
 
+        [SerializeField] private GameObject worldObject;
+        [SerializeField] private GameObject interactionObjects;
+        SphereCollider sphereCollider;
+
         private int _maxPlayersOptionValue;
         private int _aiLevelOptionValue;
         private int _npcRatioOptionValue;
         
         private void Start()
         {
+            sphereCollider = worldObject.GetComponent<SphereCollider>();
+
+            gameStartButton.onClick.AddListener(OnGameStartButtonClick);
+
             gameSettingButton.onClick.AddListener(OnSettingButtonClick);
             confirmButton.onClick.AddListener(OnConfirmButtonClick);
             cancelButton.onClick.AddListener(OnCancelButtonClick);
@@ -44,6 +53,26 @@ namespace UI.GameSetting
             roomNameInputField.placeholder.GetComponent<TMP_Text>().text = roomName;
             
             gameObject.SetActive(false);
+        }
+
+        public void OnGameStartButtonClick()
+        {
+            var objects = Instantiate(interactionObjects, Return_RandomPosition(), Quaternion.identity);
+        }
+
+        Vector3 Return_RandomPosition()
+        {
+            Vector3 originPosition = interactionObjects.transform.position;
+            // 콜라이더의 사이즈를 가져오는 bound.size 사용
+            float range_X = sphereCollider.bounds.size.x;
+            float range_Z = sphereCollider.bounds.size.z;
+
+            range_X = UnityEngine.Random.Range((range_X / 2) * -1, range_X / 2);
+            range_Z = UnityEngine.Random.Range((range_Z / 2) * -1, range_Z / 2);
+            Vector3 RandomPostion = new Vector3(range_X, 0f, range_Z);
+
+            Vector3 respawnPosition = originPosition + RandomPostion;
+            return respawnPosition;
         }
 
         public void OnSettingButtonClick()
