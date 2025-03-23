9eb60a45167e73debd6ddd10629bcda384ab8586
SOJA_Dev <rudcksdid@naver.com>
2025-03-24 07:17:11 +0900

    [chore] : getting rid of useless parts

--- Assets/Scripts/Interactions/InteractionController.cs
+++ Assets/Scripts/Interactions/InteractionController.cs
@@ -5,49 +5,30 @@ using System;
 
 public class InteractionController : MonoBehaviour
 {
-    [SerializeField] private Button gameStartButton;
     [SerializeField] private GameObject worldObject;
     [SerializeField] private GameObject interactionObjects;
     public SphereCollider SphereCollider;
 
     private void Start()
     {
-        SphereCollider = worldObject.GetComponentInChildren<SphereCollider>();
-
-        gameStartButton.onClick.AddListener(OnGameStartButtonClick);
+        SphereCollider = worldObject.GetComponent<SphereCollider>();
     }
-    
-    public void OnGameStartButtonClick()
-    {
-        Vector3[] points = GenerateFibonacciSphere(10);
 
-        foreach(Vector3 point in points)
-        {
-            //Debug.Log("point : " + point);
-            Vector3 spawnPosition = SphereCollider.transform.position + point * SphereCollider.radius * Ints.SCALE_OF_WORLD; // becuase of 'scale of the world', it needs multiplication with actual radius scale
-            //Debug.Log("spawnPosition : " + spawnPosition);
-            Instantiate(interactionObjects, spawnPosition, Quaternion.LookRotation(point));
-        }
-
-        //GameObject objects = Instantiate(interactionObjects, GetRandomPositionOnSphere(SphereCollider.center, SphereCollider.radius), Quaternion.identity);
-        //StartCoroutine(PauseObjectsRoutine(objects));
-    }
     /// <summary>
-    /// Getting random position to spawn objects on the surface of the World map
+    /// Function that creates interaction objects in random position
     /// </summary>
-    /// <param name="numPoints">상호작용 오브젝트 개수</param>
-    /// <returns>구 형태의 맵 표면</returns>
-    public static Vector3[] GenerateFibonacciSphere(int numPoints)
+    /// <param name="numberOfObjects">number of interaction objects</param>
+    public void GetRandomPositionForInteractionObjects(int numberOfObjects)
     {
-        float goldenRatio = (Mathf.Sqrt(5.0f) + 1.0f) / 2.0f; // 황금 비율 (1.618...)
-        float goldenAngle = (2.0f - goldenRatio) * (2.0f * Mathf.PI); // 황금각 (2.399...)
+        float goldenRatio = (Mathf.Sqrt(5.0f) + 1.0f) / 2.0f;
+        float goldenAngle = (2.0f - goldenRatio) * (2.0f * Mathf.PI);
 
-        Vector3[] points = new Vector3[numPoints];
+        Vector3[] points = new Vector3[numberOfObjects];
 
-        for (int i = 1; i <= numPoints; i++)
+        for (int i = 1; i <= numberOfObjects; i++)
         {
-            float lat = Mathf.Asin(-1.0f + 2.0f * (i / (float)(numPoints + 1))); // 위도 계산
-            float lon = goldenAngle * i; // 경도 계산
+            float lat = Mathf.Asin(-1.0f + 2.0f * (i / (float)(numberOfObjects + 1)));
+            float lon = goldenAngle * i;
 
             float x = Mathf.Cos(lon) * Mathf.Cos(lat);
             float y = Mathf.Sin(lon) * Mathf.Cos(lat);
@@ -56,6 +37,10 @@ public class InteractionController : MonoBehaviour
             points[i - 1] = new Vector3(x, y, z);
         }
 
-        return points;
+        foreach (Vector3 point in points)
+        {
+            Vector3 spawnPosition = SphereCollider.transform.position + point * SphereCollider.radius * Ints.SCALE_OF_WORLD;
+            Instantiate(interactionObjects, spawnPosition, Quaternion.LookRotation(point));
+        }
     }
 }
