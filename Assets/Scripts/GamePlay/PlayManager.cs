c7e99dfc08f6d651386115604e5f01c2122640df
mjmj1 <mjong3365@gmail.com>
2025-05-10 15:43:09 +0900

    fix: 위치 랜덤 설정 rpc 구현

--- Assets/Scripts/GamePlay/PlayManager.cs
+++ Assets/Scripts/GamePlay/PlayManager.cs
@@ -1,42 +1,81 @@
+using System;
 using Unity.Netcode;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace GamePlay
 {
     public class PlayManager : NetworkBehaviour
     {
         [SerializeField] private float spawnRadius = 7.5f;
-        
-        public override void OnNetworkSpawn()
+
+        public void Update()
         {
-            if (!IsSessionOwner) return;
-            
-            MoveAllPlayersToRandomSpawn();
-        }
-        
-        private void MoveAllPlayersToRandomSpawn()
-        {
-            foreach (var (clientId, value) in NetworkManager.Singleton.ConnectedClients)
+            if (IsClient)
             {
-                if (value.PlayerObject != null)
+                if (Input.GetKeyDown(KeyCode.R))
                 {
-                    MovePlayerRpc(clientId);
-                    // value.PlayerObject.transform.position = randomPos;
+                    PingToAuthorityRpc();    
+                }
+                else if (Input.GetKeyDown(KeyCode.T))
+                {
+                    PingToNotAuthorityRpc();
+                }
+                else if (Input.GetKeyDown(KeyCode.Y))
+                {
+                    PingToEveryoneRpc();
                 }
             }
         }
-        
-        [Rpc(SendTo.Server)]
-        private void MovePlayerRpc(ulong clientId)
+
+        public override void OnNetworkSpawn()
         {
-            var randomPos = GetRandomPosition();
+            if (!IsSessionOwner) return;
+
+            MoveAllPlayersToRandomSpawn();
+        }
+
+        [Rpc(SendTo.Authority)]
+        private void PingToAuthorityRpc()
+        {
+            print($"Ping SendTo.Authority");
             
-            var obj = NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject;
-            obj.transform.position = randomPos;
-            
-            Debug.Log($"Client {clientId}: Position = {randomPos}");
+            print($"Ping SendTo.Authority from Client {NetworkManager.Singleton.LocalClientId}");
         }
         
+        [Rpc(SendTo.NotAuthority)]
+        private void PingToNotAuthorityRpc()
+        {
+            print($"Ping SendTo.NotAuthority");
+            
+            print($"Ping SendTo.NotAuthority from Client {NetworkManager.Singleton.LocalClientId}");
+        }
+        
+        [Rpc(SendTo.Everyone)]
+        private void PingToEveryoneRpc()
+        {
+            print($"Ping SendTo.Everyone");
+            
+            print($"Ping SendTo.Everyone from Client {NetworkManager.Singleton.LocalClientId}");
+        }
+
+        private void MoveAllPlayersToRandomSpawn()
+        {
+            MoveRandomPositionRpc();
+        }
+
+        [Rpc(SendTo.Everyone)]
+        private void MoveRandomPositionRpc()
+        {
+            var clientId = NetworkManager.Singleton.LocalClientId;
+            var randomPos = GetRandomPosition();
+
+            var obj = NetworkManager.Singleton.ConnectedClients[clientId].PlayerObject;
+            obj.transform.position = randomPos;
+
+            print($"Client {clientId}: Position = {randomPos}");
+        }
+
         private Vector3 GetRandomPosition()
         {
             return Random.onUnitSphere.normalized * spawnRadius;
