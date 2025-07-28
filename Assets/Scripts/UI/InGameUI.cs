40bab7c7d74f489afa6d8ff62578800b08ba61bc
mjmj1 <mjong3365@gmail.com>
2025-07-29 00:09:41 +0900

    feat: 플레이어 체력 UI

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -1,26 +1,50 @@
 using GamePlay;
+using Players;
 using TMPro;
+using UI.Scriptable;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class InGameUI : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI timerText;
+        [SerializeField] private Image[] redHealth;
+        [SerializeField] private HpImageData hpImageData;
 
         private void Start()
         {
-            PlayManager.Instance.currentTime.OnValueChanged += OnValueChanged;
+            PlayManager.Instance.currentTime.OnValueChanged += OnTimerChanged;
+            NetworkManager.Singleton.LocalClient.PlayerObject
+                .GetComponent<PlayerEntity>().health.OnValueChanged += OnPlayerHealthChanged;
+
+            NetworkManager.OnDestroying += OnDestroying;
         }
 
-        private void OnDestroy()
+        private void OnDestroying(NetworkManager obj)
         {
-            PlayManager.Instance.currentTime.OnValueChanged -= OnValueChanged;
+            NetworkManager.OnDestroying -= OnDestroying;
+
+            PlayManager.Instance.currentTime.OnValueChanged -= OnTimerChanged;
+            NetworkManager.Singleton.LocalClient.PlayerObject
+                .GetComponent<PlayerEntity>().health.OnValueChanged -= OnPlayerHealthChanged;
         }
 
-        private void OnValueChanged(int previousValue, int newValue)
+        private void OnTimerChanged(int previousValue, int newValue)
         {
             timerText.text = $"{newValue / 60:00}:{newValue % 60:00}";
+        }
+
+        private void OnPlayerHealthChanged(int oldValue, int newValue)
+        {
+            var value = newValue;
+
+            foreach (var item in redHealth)
+            {
+                item.sprite = value-- > 0 ? hpImageData.hpSprites[1] : hpImageData.hpSprites[0];
+            }
         }
     }
 }
