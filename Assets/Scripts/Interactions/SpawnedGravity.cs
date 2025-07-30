4d8a45f25830d2cb458b859e221dc31e58e1289e
SOJA_Dev <rudcksdid@naver.com>
2025-07-30 15:48:49 +0900

    [feat]: dropped apple
    이전 커밋 때 버그 고치면서 구현함

--- Assets/Scripts/Interactions/SpawnedGravity.cs
+++ Assets/Scripts/Interactions/SpawnedGravity.cs
@@ -5,24 +5,29 @@ public class SpawnedGravity : MonoBehaviour
 {
     private Rigidbody rb;
     public float rotationSpeed = 50f;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
+
     private void Start()
     {
         InitializeGravity();
     }
+
     private void Update()
     {
         AlignToSurface();
     }
+
     private void InitializeGravity()
     {
         rb.useGravity = !PlanetGravity.Instance;
 
         PlanetGravity.Instance?.Subscribe(rb);
     }
+
     private void AlignToSurface()
     {
         if (!PlanetGravity.Instance) return;
