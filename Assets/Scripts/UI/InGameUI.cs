d7c243a2c6e9d70fd094872a089de8b8da1c7997
SOJA_Dev <rudcksdid@naver.com>
2025-08-06 15:47:27 +0900

    [graphic]: hit slow effect

--- Assets/Scripts/UI/InGameUI.cs
+++ Assets/Scripts/UI/InGameUI.cs
@@ -1,6 +1,7 @@
 using GamePlay;
 using Players;
 using Scriptable;
+using System.Collections;
 using TMPro;
 using Unity.Netcode;
 using UnityEngine;
@@ -13,6 +14,8 @@ namespace UI
         [SerializeField] private TextMeshProUGUI timerText;
         [SerializeField] private Image[] redHealth;
         [SerializeField] private HpImageData hpImageData;
+        [SerializeField] private Image hitOverlay;
+        [SerializeField] private float fadeDuration = 0.2f;
 
         private void Start()
         {
@@ -49,6 +52,28 @@ namespace UI
             {
                 item.sprite = value-- > 0 ? hpImageData.hpSprites[1] : hpImageData.hpSprites[0];
             }
+            ShowHitEffect();
+        }
+
+        public void ShowHitEffect()
+        {
+            StopAllCoroutines();
+            StartCoroutine(Flash());
+        }
+        private IEnumerator Flash()
+        {
+            hitOverlay.color = new Color(1, 0, 0, 0.5f);
+
+            float elapsed = 0f;
+            while (elapsed < fadeDuration)
+            {
+                elapsed += Time.deltaTime;
+                float alpha = Mathf.Lerp(0.5f, 0, elapsed / fadeDuration);
+                hitOverlay.color = new Color(1, 0, 0, alpha);
+                yield return null;
+            }
+
+            hitOverlay.color = new Color(1, 0, 0, 0);
         }
     }
 }
